1146da762e09e4f477759f1dcec406b62702b39f
George <george_lee@brown.edu>
2019-10-27 20:58:52 -0400

    added routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import "./App.css";
 import SponsorshipPage from "./pages/SponsorshipPage/SponsorshipPage/SponsorshipPage";
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 const App: React.FC = () => {
     return (
-        <div className="App">
-            <SponsorshipPage msg="this is how to use props in typescript" />
-        </div>
+        <Router>
+            {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+            <Switch>
+                <Route path="/sponsors">
+                    <div className="App">
+                        <SponsorshipPage msg="this is how to use props in typescript" />
+                    </div>
+                </Route>
+                <Route path="/">
+                    <div className="App">
+                        <SponsorshipPage msg="this is how to use props in typescript" />
+                    </div>
+                </Route>
+            </Switch>
+        </Router>
     );
 };
 
