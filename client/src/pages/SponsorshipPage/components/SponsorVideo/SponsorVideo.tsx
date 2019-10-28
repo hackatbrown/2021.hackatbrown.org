d55635e8fc42e6b635d7c4b8ff70a86ee5f907e1
George <george_lee@brown.edu>
2019-10-27 21:06:01 -0400

    allowed video controls in sponsorship site

--- client/src/pages/SponsorshipPage/components/SponsorVideo/SponsorVideo.tsx
+++ client/src/pages/SponsorshipPage/components/SponsorVideo/SponsorVideo.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import "./SponsorVideo.css";
-import ReactPlayer from 'react-player'
+import ReactPlayer from "react-player";
 
 /**
  * define a type model for the props you are passing in to the component
  */
-type SponsorVideoProps = {
-};
+type SponsorVideoProps = {};
 
 /**
  * define a type model for the state of the page
  */
-type SponsorVideoState = {
-};
+type SponsorVideoState = {};
 
 export default class SponsorVideo extends React.Component<
     SponsorVideoProps,
@@ -23,9 +21,14 @@ export default class SponsorVideo extends React.Component<
     }
     render() {
         return (
-          <div className="sponsor-vid">
-            <ReactPlayer id="sponsor-promo" url='https://www.youtube.com/watch?v=mOw8sz48-T8&feature=youtu.be' playing/>
-          </div>
+            <div className="sponsor-vid">
+                <ReactPlayer
+                    id="sponsor-promo"
+                    url="https://www.youtube.com/watch?v=mOw8sz48-T8&feature=youtu.be"
+                    playing
+                    controls
+                />
+            </div>
         );
     }
 }
