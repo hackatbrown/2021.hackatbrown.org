046546b4f4dce6d2fa02bc6bc2d7c9a1b49ce551
Lauren Choi <laurenchoi@gmail.com>
2019-11-01 15:42:25 -0400

    created basic email page
    Josh and I created a new React/Typescript project named emailpage. The emails are stored in an array with each submission.

---
+++ src/pages/emailpage/src/App.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+interface IState {
+  currentTask: string;
+  tasks: Array<string>;
+}
+
+export class App extends React.Component<{}, IState> {
+
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      currentTask: "",
+      tasks: []
+    };
+  }
+
+  handleSubmit(e: any) {
+    e.preventDefault();
+    this.setState({
+      currentTask: "",
+      tasks: [
+        ...this.state.tasks,
+        this.state.currentTask
+      ]
+    })
+  }
+
+render() {
+  console.log(this.state);
+  return (
+    <div>
+      <h1>Hack@Brown 2020 Coming Soon!</h1>
+      <form onSubmit={e => this.handleSubmit(e)}>
+        <input
+          type="text"
+          placeholder="Enter your email here to get updated!"
+          value={this.state.currentTask}
+          onChange={e => this.setState({currentTask: e.target.value})} />
+        <button type="submit">Submit</button>
+        </form>
+    </div>
+  )
+}
+
+}
+
+export default App;
