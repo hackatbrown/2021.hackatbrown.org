c6502e32d33df3e29c767341e5c166bdc5f5583e
Brandon Lee <44144566+brandonqlee@users.noreply.github.com>
2019-11-29 18:19:24 -0500

    add modal to landing

--- src/pages/LandingPage/LandingPageComponents/Toolbar/Toolbar.tsx
+++ src/pages/LandingPage/LandingPageComponents/Toolbar/Toolbar.tsx
@@ -4,11 +4,14 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import logoImg from "../../../../assets/images/LandingPage/placeholder-square.png";
+import Firebase from "../../../../components/Firebase";
 
-type ToolbarProps = {};
+type ToolbarProps = {
+    firebase: (Firebase | null)
+};
 
 type ToolbarState = {
-    state: string;
+    user: any
 };
 
 export default class Toolbar extends React.Component<
@@ -18,10 +21,37 @@ export default class Toolbar extends React.Component<
     constructor(props: ToolbarProps) {
         super(props);
         this.state = {
-            state: "good"
+            user: null
         };
     }
+
+    // Workaround of having nothing as else case if firebase is null
+    doNothing = () => {}
+
+    // Check if user is logged in when component mounts
+    componentDidMount = () => {
+        let currFirebase = this.props.firebase;
+        if (currFirebase == null) { // if true, error
+
+        } else {
+            currFirebase.doAuthListener(this); // check if user is logged in or not
+        }
+    }
+
     render() {
+        let button; // display login/join or dashboard button
+        if (this.state.user != null) { // if true, user is logged in
+            // TODO: Go to dashboard
+            button =
+                <button onClick={(this.props.firebase == null) ? this.doNothing : this.props.firebase.doLogOut} className="toolbar-signin">
+                    <p>Log out</p>
+                </button>
+        } else { // else, user is not logged in
+            // button =
+            //     <button onClick={(this.props.firebase == null) ? this.doNothing : this.props.firebase.doLogOut} className="toolbar-signin">
+            //                         <p>Join/Login</p>
+            //                     </button>
+        }
         return (
             <div className="toolbar">
                 <Container>
@@ -58,9 +88,7 @@ export default class Toolbar extends React.Component<
                             </Col>
 
                             <Col>
-                                <div className="toolbar-signin">
-                                    <p>Join/Login</p>
-                                </div>
+                                {button}
                             </Col>
                         </div>
                     </Row>
