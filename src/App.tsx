4850a31a32b6ce4cea945a21b23657c9d372a177
George <george_lee@brown.edu>
2019-12-02 21:02:35 -0500

    added google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
 import React from "react";
 import "./App.css";
 import SponsorshipPage from "./pages/SponsorshipPage/SponsorshipPage";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import EmailPage from "./pages/emailpage/EmailPage";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import ReactGA from "react-ga";
+import { createBrowserHistory } from "history";
+import { Router, Switch, Route } from "react-router-dom";
+
+const history = createBrowserHistory();
 
 const trackingId = "UA-153759329-1"; // Replace with your Google Analytics tracking ID
 ReactGA.initialize(trackingId);
 ReactGA.pageview("/");
-ReactGA.set({
-    // any data that is relevant to the user session
-    // that you would like to track with google analytics
+// Initialize google analytics page view tracking
+history.listen(location => {
+    ReactGA.set({ page: location.pathname }); // Update the user's current page
+    ReactGA.pageview(location.pathname); // Record a pageview for the given page
 });
 
 const App: React.FC = () => {
     return (
-        <Router>
+        <Router history={history}>
             {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
             <Switch>
