ed45b6548e8fc18235f0185eb3588c2abab5645c
George <george_lee@brown.edu>
2019-12-02 20:20:19 -0500

    added firebase import and added google analytics?

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,15 @@ import SponsorshipPage from "./pages/SponsorshipPage/SponsorshipPage";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import EmailPage from "./pages/emailpage/EmailPage";
 import LandingPage from "./pages/LandingPage/LandingPage";
+import ReactGA from "react-ga";
+
+const trackingId = "UA-153759329-1"; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
+ReactGA.pageview("/");
+ReactGA.set({
+    // any data that is relevant to the user session
+    // that you would like to track with google analytics
+});
 
 const App: React.FC = () => {
     return (
--- src/pages/LandingPage/LandingPageComponents/Toolbar/Toolbar.tsx
+++ src/pages/LandingPage/LandingPageComponents/Toolbar/Toolbar.tsx
@@ -3,15 +3,16 @@ import "./Toolbar.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-
 import logoImg from "../../../../assets/images/LandingPage/whitepaw.png";
 import { Link, animateScroll as scroll } from "react-scroll";
+import Firebase from "../../../../components/Firebase";
+
 type ToolbarProps = {
-    firebase: (Firebase | null)
+    firebase: Firebase | null;
 };
 
 type ToolbarState = {
-    user: any
+    user: any;
 };
 
 export default class Toolbar extends React.Component<
@@ -25,34 +26,42 @@ export default class Toolbar extends React.Component<
         };
     }
 
-
     scrollToTop = () => {
         scroll.scrollToTop();
-      };
+    };
 
     // Workaround of having nothing as else case if firebase is null
-    doNothing = () => {}
+    doNothing = () => {};
 
     // Check if user is logged in when component mounts
     componentDidMount = () => {
         let currFirebase = this.props.firebase;
-        if (currFirebase == null) { // if true, error
-
+        if (currFirebase == null) {
+            // if true, error
         } else {
             currFirebase.doAuthListener(this); // check if user is logged in or not
         }
-    }
-
+    };
 
     render() {
         let button; // display login/join or dashboard button
-        if (this.state.user != null) { // if true, user is logged in
+        if (this.state.user != null) {
+            // if true, user is logged in
             // TODO: Go to dashboard
-            button =
-                <button onClick={(this.props.firebase == null) ? this.doNothing : this.props.firebase.doLogOut} className="toolbar-signin">
+            button = (
+                <button
+                    onClick={
+                        this.props.firebase == null
+                            ? this.doNothing
+                            : this.props.firebase.doLogOut
+                    }
+                    className="toolbar-signin"
+                >
                     <p>Log out</p>
                 </button>
-        } else { // else, user is not logged in
+            );
+        } else {
+            // else, user is not logged in
             // button =
             //     <button onClick={(this.props.firebase == null) ? this.doNothing : this.props.firebase.doLogOut} className="toolbar-signin">
             //                         <p>Join/Login</p>
@@ -63,39 +72,77 @@ export default class Toolbar extends React.Component<
                 <Container>
                     <Row>
                         <Col>
-                        <div className="toolbar-logo">
-                            <img id="logo-img" src={logoImg} onClick={this.scrollToTop}></img>
-                        </div>
+                            <div className="toolbar-logo">
+                                <img
+                                    id="logo-img"
+                                    src={logoImg}
+                                    onClick={this.scrollToTop}
+                                ></img>
+                            </div>
                         </Col>
 
                         <div className="toolbar-items">
-                        <Col>
-                        <div className="toolbar-about">
-                            <Link activeClass="active" to="intro" spy={true} smooth={true} offset={-70} duration={500}>About</Link>
-                        </div>
-                        </Col>
-
-                        <Col>
-                        <div className="toolbar-itinerary">
-                        <Link activeClass="active" to="itinerary" spy={true} smooth={true} offset={-70} duration={500}>Itinerary</Link>
-                        </div>
-                        </Col>
-
-                        <Col>
-                        <div className="toolbar-faq">
-                        <Link activeClass="active" to="faq" spy={true} smooth={true} offset={-70} duration={500}>FAQ</Link>
-                        </div>
-                        </Col>
-
-                        <Col>
-                        <div className="toolbar-sponsors">
-                        <Link activeClass="active" to="sponsors" spy={true} smooth={true} offset={-70} duration={500}>Sponsors</Link>
-                        </div>
-                        </Col>
+                            <Col>
+                                <div className="toolbar-about">
+                                    <Link
+                                        activeClass="active"
+                                        to="intro"
+                                        spy={true}
+                                        smooth={true}
+                                        offset={-70}
+                                        duration={500}
+                                    >
+                                        About
+                                    </Link>
+                                </div>
+                            </Col>
 
                             <Col>
-                                {button}
+                                <div className="toolbar-itinerary">
+                                    <Link
+                                        activeClass="active"
+                                        to="itinerary"
+                                        spy={true}
+                                        smooth={true}
+                                        offset={-70}
+                                        duration={500}
+                                    >
+                                        Itinerary
+                                    </Link>
+                                </div>
                             </Col>
+
+                            <Col>
+                                <div className="toolbar-faq">
+                                    <Link
+                                        activeClass="active"
+                                        to="faq"
+                                        spy={true}
+                                        smooth={true}
+                                        offset={-70}
+                                        duration={500}
+                                    >
+                                        FAQ
+                                    </Link>
+                                </div>
+                            </Col>
+
+                            <Col>
+                                <div className="toolbar-sponsors">
+                                    <Link
+                                        activeClass="active"
+                                        to="sponsors"
+                                        spy={true}
+                                        smooth={true}
+                                        offset={-70}
+                                        duration={500}
+                                    >
+                                        Sponsors
+                                    </Link>
+                                </div>
+                            </Col>
+
+                            <Col>{button}</Col>
                         </div>
                     </Row>
                 </Container>
