0e3a0c3f4e8c0b5ad37e8a2c5ef107d83ec3f84b
Brandon Lee <44144566+brandonqlee@users.noreply.github.com>
2019-12-09 00:52:41 -0500

    check spam

--- src/pages/LandingPage/LandingPageComponents/LoginJoin/LoginJoin.tsx
+++ src/pages/LandingPage/LandingPageComponents/LoginJoin/LoginJoin.tsx
@@ -252,7 +252,7 @@ export default class LoginJoin extends Component<
         }
         session.setState({
             message:
-                "If given email was found, you will receive your password reset email shortly."
+                "If given email was found, you will receive your password reset email shortly; make sure to check your spam folder!"
         });
     };
 
@@ -295,7 +295,7 @@ export default class LoginJoin extends Component<
                             aria-describedby="emailHelp"
                         />
                     </div>
-                    <div className="error-message">{this.state.message}</div>
+                    <div className="info-message">{this.state.message}</div>
                     <div className="button-row">
                         <button
                             type="submit"
