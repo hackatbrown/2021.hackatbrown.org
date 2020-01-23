4499f76b7bba447de60c3c84c4e09a9b2f0ef73f
Brandon Lee <44144566+brandonqlee@users.noreply.github.com>
2020-01-22 19:59:28 -0500

    responsive dayof page

--- src/pages/DayOfPage/components/Itinerary/Itinerary.tsx
+++ src/pages/DayOfPage/components/Itinerary/Itinerary.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import "./Itinerary.css";
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import "./Itinerary.css";
 import sayles0 from "../../../../assets/images/DayOf/sayles0.png";
 import sayles1 from "../../../../assets/images/DayOf/sayles1.png";
 import sayles2 from "../../../../assets/images/DayOf/sayles2.png";
@@ -33,40 +33,42 @@ export default class Itinerary extends React.Component<
             <div className="dayof-itinerary">
               <Container>
                 <Row>
-                  <Col sm>
-                    <table>
-                        <tr>
-                            <th className="day">SATURDAY</th>
-                        </tr>
-                        <tr>
-                            <td>9:00am<br/>Registration & Sponsor Fair<br/><div className="caption">Check in at Friedman Lobby,<br/>or Check in at Sayles Hall 108</div></td>
-                        </tr>
-                        <tr>
-                            <td>11:00am – 12:00pm<br/>Kickoff & Keynote Speech<br/><div className="caption">Head to Salomon Hall DECI</div></td>
-                        </tr>
-                        <tr>
-                            <td>12:00pm – 1:00pm<br/>Lunch & Team Forming<br/><div className="caption">Sayles Hall & Friedman Hall 102</div></td>
-                        </tr>
-                        <tr>
-                            <td>1:00pm<br/>Hacking begins!<br/><div className="caption">Sayles Hall<br/>Friedman 201, 202, 208</div></td>
-                        </tr>
-                        <tr>
-                            <td>1:30pm – 2:30pm<br/>iOS Workshop<br/><div className="caption">Friedman 108</div></td>
-                        </tr>
-                        <tr>
-                            <td>2:30pm – 3:30pm<br/>Android Workshop<br/><div className="caption">Friedman 108</div></td>
-                        </tr>
-                        <tr>
-                            <td>4:00pm – 5:00pm<br/>Javascript Workshop<br/><div className="caption">Friedman 108</div></td>
-                        </tr>
-                        <tr>
-                            <td>6:00pm – 7:00pm<br/>Dinner<br/><div className="caption">Sayles Hall</div></td>
-                        </tr>
-                        <tr>
-                            <td>10:30pm<br/><div className="caption">Late Night Surprise!</div></td>
-                        </tr>
-                    </table>
-                  </Col>
+                <div id="saturday">
+                    <Col sm>
+                        <table>
+                            <tr>
+                                <th className="day">SATURDAY</th>
+                            </tr>
+                            <tr>
+                                <td>9:00am<br/>Registration & Sponsor Fair<br/><div className="caption">Check in at Friedman Lobby,<br/>or Check in at Sayles Hall 108</div></td>
+                            </tr>
+                            <tr>
+                                <td>11:00am – 12:00pm<br/>Kickoff & Keynote Speech<br/><div className="caption">Head to Salomon Hall DECI</div></td>
+                            </tr>
+                            <tr>
+                                <td>12:00pm – 1:00pm<br/>Lunch & Team Forming<br/><div className="caption">Sayles Hall & Friedman Hall 102</div></td>
+                            </tr>
+                            <tr>
+                                <td>1:00pm<br/>Hacking begins!<br/><div className="caption">Sayles Hall<br/>Friedman 201, 202, 208</div></td>
+                            </tr>
+                            <tr>
+                                <td>1:30pm – 2:30pm<br/>iOS Workshop<br/><div className="caption">Friedman 108</div></td>
+                            </tr>
+                            <tr>
+                                <td>2:30pm – 3:30pm<br/>Android Workshop<br/><div className="caption">Friedman 108</div></td>
+                            </tr>
+                            <tr>
+                                <td>4:00pm – 5:00pm<br/>Javascript Workshop<br/><div className="caption">Friedman 108</div></td>
+                            </tr>
+                            <tr>
+                                <td>6:00pm – 7:00pm<br/>Dinner<br/><div className="caption">Sayles Hall</div></td>
+                            </tr>
+                            <tr>
+                                <td>10:30pm<br/><div className="caption">Late Night Surprise!</div></td>
+                            </tr>
+                        </table>
+                    </Col>
+                </div>
                   <Col sm>
                     <table>
                         <tr>
