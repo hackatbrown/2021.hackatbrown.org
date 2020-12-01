d44d31005b26f400911c8edf4f92a67b00e7b2c7
Wenhuang Zeng <wenhuang_zeng@brown.edu>
2020-12-01 17:54:17 -0500

    removed SponsorsList

--- src/pages/SponsorshipPage/SponsorshipPage.tsx
+++ src/pages/SponsorshipPage/SponsorshipPage.tsx
@@ -1,15 +1,10 @@
 import React from "react";
 import "./SponsorshipPage.css";
 import SponsorHome from "./components/SponsorHome/SponsorHome";
-import SponsorVideo from "./components/SponsorVideo/SponsorVideo";
 import WhySponsor from "./components/WhySponsor/WhySponsor";
 import SponsorTiers from "./components/SponsorTiers/SponsorTiers";
-<<<<<<< HEAD
-import SponsorsList from "./components/SponsorsList/SponsorsList";
-=======
 import Footer from "../LandingPage/LandingPageComponents/Footer/Footer";
 
->>>>>>> 42ec899f8ca3c401d1b5ac704cdfa16ac03b094e
 /**
  * define a type model for the props you are passing in to the component
  */
@@ -39,11 +34,7 @@ export default class SponsorshipPage extends React.Component<
                 {/* <SponsorVideo /> */}
                 <WhySponsor />
                 <SponsorTiers />
-<<<<<<< HEAD
-                <SponsorsList />
-=======
                 <Footer />
->>>>>>> 42ec899f8ca3c401d1b5ac704cdfa16ac03b094e
             </div>
         );
     }
