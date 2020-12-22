a65529976fdb47aef8ed01352e7e4a541d12a3c5
Wenhuang Zeng <wenhuang_zeng@brown.edu>
2020-12-22 17:50:38 -0500

    updated intro section responsive styling

--- src/pages/LandingPage/LandingPageComponents/Intro/Intro.tsx
+++ src/pages/LandingPage/LandingPageComponents/Intro/Intro.tsx
@@ -22,33 +22,33 @@ export default class Intro extends React.Component<IntroProps, IntroState> {
         return (
             <div className="intro">
             <Container>
-                    <Row>
-                    <Col sm={6}>
-            <div className="juice">
-                <img id="juiceImg" src={juiceImg}></img>
-            </div>
-            </Col>
-            <Col sm={1}></Col>
-                        <Col sm={5}>
-                <div className="intro-blurb">
-                    <h1 className="intro-blurb-header">
-                        Welcome to H@B!
-                    </h1>
-                    <p className="intro-blurb-text">
-                        Hack@Brown is all about learning. Spend a <span className='bold'>virtual</span> weekend
-                        with 500 other students to take a step out of your comfort
-                        zone!
-                        <br/><br/>
-                        Whether you’re building your first website, dabbling in Photoshop,
-                        or hacking with an Oculus Rift, Hack@Brown is for individuals of
-                        all backgrounds.
-                        <br/><br/>
-                        This year, our theme is <span className='bold'>"After School." </span>
-                        Take inspiration from your passions and experiment with your wildest ideas.
-                        Build away!
-                    </p>
-                </div>
-                </Col>
+                    <Row className="align-items-center">
+                        <Col sm={5} className="col-md-5 order-md-3">
+                        <div className="intro-blurb">
+                            <h1 className="intro-blurb-header">
+                                Welcome to H@B!
+                            </h1>
+                            <p className="intro-blurb-text">
+                                Hack@Brown is all about learning. Spend a <span className='bold'>virtual</span> weekend
+                                with 500 other students to take a step out of your comfort
+                                zone!
+                                <br/><br/>
+                                Whether you’re building your first website, dabbling in Photoshop,
+                                or hacking with an Oculus Rift, Hack@Brown is for individuals of
+                                all backgrounds.
+                                <br/><br/>
+                                This year, our theme is <span className='bold'>"After School." </span>
+                                Take inspiration from your passions and experiment with your wildest ideas.
+                                Build away!
+                            </p>
+                        </div>
+                        </Col>
+                        <Col sm={6} className="col-md-6 order-md-2">
+                            <div className="juice">
+                                <img id="juiceImg" src={juiceImg}></img>
+                            </div>
+                        </Col>
+                        <Col sm={1} className="col-md-1 order-md-2"></Col>
                     </Row>
                 </Container>
             </div>
