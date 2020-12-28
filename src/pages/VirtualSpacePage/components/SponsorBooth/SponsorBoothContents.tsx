1508fde6ea5dacc05d593840bffff4c1441710e4
edoss00 <edoss00@stuy.edu>
2020-12-28 15:37:03 -0500

    updated sponsor, no hard code

--- src/pages/VirtualSpacePage/components/SponsorBooth/SponsorBoothContents.tsx
+++ src/pages/VirtualSpacePage/components/SponsorBooth/SponsorBoothContents.tsx
@@ -1,9 +1,15 @@
 export default {
-    "bloomberg": {
+  "bloomberg": {
         "description": "this is the description",
         "url": "this is the url",
         "tier": "this is the tier",
         "asset_path": "this is the asset path"
+    },
+
+    'facebook' : {
+        "description": "Facebook is a social media company lorem ipsum",
+        "url": "url",
+        "tier": "kilo",
+        "asset_path": "../../../../assets/images/LandingPage/companylogo/facebook.png"
     }
-    // Add more as we get more sponsors confirmed
 }
--- src/pages/VirtualSpacePage/components/SponsorBooth/SponsorBoothModal.tsx
+++ src/pages/VirtualSpacePage/components/SponsorBooth/SponsorBoothModal.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./SponsorBoothModal.css";
-import facebooklogo from "../../../../assets/images/LandingPage/companylogo/facebook.png";
 
 type SponsorBoothModalProps = {
-    sponsorInfo : Object // This can be changed
+    sponsorDescription: string;
+    sponsorURL: string;
+    sponsorTier: string;
+    sponsorAsset: string;
 };
 
 type SponsorBoothModalState = {
@@ -18,12 +20,17 @@ export default class SponsorBoothModal extends React.Component<
     }
 
     render() {
+      const description = this.props.sponsorDescription;
+      const url = this.props.sponsorURL;
+      const tier = this.props.sponsorTier;
+      const asset = this.props.sponsorAsset;
         return (
             <div id='modal'>
-                <img id = "facebooklogo" src ={facebooklogo}></img><br></br>
-                <p> Facebook is a social media company lorem ipsum</p><br></br>
-                <p><b>Book a coffee chat below!</b></p><br></br>
-                <iframe src="https://calendar.google.com/calendar/embed?height=312&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FLos_Angeles&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%230B8043&amp;showTitle=0&amp;mode=WEEK&amp;showPrint=0&amp;showTabs=1" style={{border: 0.6 }} width="691" height="312" frameBorder="0" scrolling="no"></iframe>
+                <div className={tier}>{tier}</div>
+                <img id ="logo" src ={asset} alt="Logo"/><br/>
+                <p> {description}</p><br/>
+                <p><b>Book a coffee chat below!</b></p><br/>
+                <iframe src="https://calendar.google.com/calendar/embed?height=312&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FLos_Angeles&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%230B8043&amp;showTitle=0&amp;mode=WEEK&amp;showPrint=0&amp;showTabs=1" style={{border: 0.6}} width="691" height="312" frameBorder="0" scrolling="no"></iframe>
             </div>
         );
     }
