a574aa5729bf07abc47acfe176a0647f27d50738
ethanga12 <66858278+ethanga12@users.noreply.github.com>
2021-01-21 23:38:56 -0600

    tables added for infobooth modal

--- src/pages/DashboardPage/components/VirtualSpace/InfoBooth/InfoBoothModal.tsx
+++ src/pages/DashboardPage/components/VirtualSpace/InfoBooth/InfoBoothModal.tsx
@@ -43,112 +43,147 @@ export default class InfoBoothModal extends React.Component<
         } else if (infoType === "Workshops") {
             bottomSection = 
                 <div className="bottom workshops">
-                    <div className="grid">
-                        <div className="dates">
+                    <div className="table">
+                        <table>
+                        <tr>
+                        <td><div className="dates grid-item">
                             <p><strong>Jan 22</strong></p>
                         </div>
-                        <div className="times">
+                        </td>
+                        <td><div className="times grid-item">
                             <p>11:30 - 12:30</p>
-                        </div>
-                        <div className="links">
+                        </div></td>
+                        <td><div className="links grid-item">
                             <a href="https://us02web.zoom.us/j/86545850944?pwd=V0pvMU5jcTV4L0lEUGkrZFlGaEx5QT09">Intro to HTML/CSS</a>
-                        </div>
-                        <div className="dates">
+                        </div></td>
+                        </tr>
+                        <tr>
+                        <td><div className="dates grid-item">
                             <p><strong> </strong></p>
                         </div>
-                        <div className="times">
+                        </td>
+                        <td><div className="times grid-item">
                             <p>13:00 - 14:00</p>
                         </div>
-                        <div className="links">
+                        </td>
+                        <td><div className="links grid-item">
                             <a href="https://us02web.zoom.us/j/83295677717?pwd=U1V4cmZRTnFKWW5JaVNPMUFBeXdEdz09">Workshop: Google Cloud</a>
                         </div>
-                        <div className="times">
+                        </td>
+                        </tr>
+                        <tr>
+                        <td><div className="dates grid-item">
+                            <p><strong> </strong></p>
+                        </div>
+                        </td>
+                        <td><div className="times grid-item">
                             <p>17:30 - 18:30</p>
                         </div>
-                        <div className="dates">
-                            <p><strong> </strong></p>
-                        </div>
-                        <div className="links">
+                        </td>
+                        <td><div className="links grid-item">
                             <a href="https://us02web.zoom.us/j/83187295277?pwd=Mk9nYnZTdDBvRkoxMElwZXJZM2J1dz09">Workshop: (MLH) Cybersecurity Cloud</a>
                         </div>
-                        <div className="times">
+                        </td>
+                        </tr>
+                        <tr>
+                        <td><div className="times grid-item">
                             <p>18:00 - 19:00</p>
-                        </div>
-                        <div className="dates">
+                        </div></td>
+                        <td><div className="dates grid-item">
                             <p><strong> </strong></p>
-                        </div>
-                        <div className="links">
+                        </div></td>
+                        <td><div className="links grid-item">
                             <a href="https://us02web.zoom.us/j/86347112241?pwd=K2ZNdmlxR2JDZ1RqbHhMb1hDL1laZz09">Workshop: Intro to Deep Learning</a>
-                        </div>
-                        <div className="times">
+                        </div></td>
+                        </tr>
+                        <tr>
+                        <td><div className="times grid-item">
                             <p>20:00 - 21:00</p>
                         </div>
-                        <div className="dates">
+                        </td>
+                        <td><div className="dates grid-item">
                             <p><strong> </strong></p>
-                        </div>
-                        <div className="links">
+                        </div></td>
+                        <td><div className="links grid-item">
                             <a href="https://us02web.zoom.us/j/86301279007?pwd=THZwY0sxeUE3S3FmTE92V2lVbTFHQT09">Design Workshop: Introduction to Figma</a>
-                        </div>
-                        <div className="dates">
+                        </div></td>
+                        </tr>
+                        <tr>
+                        <td><div className="dates grid-item">
                             <p><strong>Jan 23</strong></p>
-                        </div>
-                        <div className="times">
+                        </div></td>
+                        <td><div className="times grid-item">
                             <p>9:30 - 10:30</p>
-                        </div>
-                        <div className="links">
+                        </div></td>
+                        <td><div className="links grid-item">
                             <a href="https://us02web.zoom.us/j/84252904233?pwd=OERDVDJwdXNMK1JXcmF3dHRUeVlSQT09">Workshop: Ethics in CS and Socially Responsible Computing</a>
-                        </div>
+                        </div></td>
+                        </tr>
+                        </table>
                     </div>
                 </div>
         } else if (infoType === "Speaker Panel") {
             bottomSection = 
                 <div className="bottom speaker-panel">
-                    <div className="grids">
-                        <div className="dates">
+                    <div className="table">
+                        <table>
+                        <tr>
+                        <td><div className="dates grid-item">
                             <p><strong>Jan 22</strong></p>
-                        </div>
-                        <div className="times">
+                        </div></td>
+                        <td><div className="times grid-item">
                             <p>12:30 - 13:30</p>
-                        </div>
-                        <div className="links">
+                        </div></td>
+                        <td><div className="links grid-item">
                             <a href="https://us02web.zoom.us/j/89725906434?pwd=c1hNbnVtUnVYSk8wa0RxdkExWGF1dz09"><strong>Talk: Professor Edgar</strong></a>
                         </div>
-                        <div className="dates">
+                        </td>
+                        </tr>
+                        <tr>
+                        <td><div className="dates grid-item">
                             <p><strong> </strong></p>
-                        </div>
-                        <div className="times">
+                        </div></td>
+                        <td><div className="times grid-item">
                             <p>14:00 - 15:00</p>
-                        </div>
-                        <div className="links">
+                        </div></td>
+                        <td><div className="links grid-item">
                             <a href="https://us02web.zoom.us/j/83381000344?pwd=UXV3TktTbEpqdXpUZURVSXoxeE9BQT09"><strong>Talk: Professor Litman</strong> The Four Ways We Tell Machines What We Want, and How We Can Do Better</a>
-                        </div>
-                        <div className="dates">
+                        </div></td>
+                        </tr>
+                        <tr>
+                        <td><div className="dates grid-item">
                             <p><strong> </strong></p>
-                        </div>
-                        <div className="times">
+                        </div></td>
+                        <td><div className="times grid-item">
                             <p>15:00 - 16:00</p>
-                        </div>
-                        <div className="links">
+                        </div></td>
+                        <td><div className="links grid-item">
                             <a href="https://us02web.zoom.us/j/84344188222?pwd=aTBwc0x2dDdOMXZwNVU1Mi91eUU3UT09"><strong>Panel: Diversity and Inclusion</strong></a>
-                        </div>
-                        <div className="dates">
+                        </div></td>
+                        </tr>
+                        <tr>
+                        <td><div className="dates grid-item">
                             <p><strong> </strong></p>
-                        </div>
-                        <div className="times">
+                        </div></td>
+                        <td><div className="times grid-item">
                             <p>16:00 - 7:00</p>
-                        </div>
-                        <div className="links">
+                        </div></td>
+                        <td><div className="links grid-item">
                             <a href="https://us02web.zoom.us/j/83877631019?pwd=N3RTbHpmSHNVaXVHdVRPMTQ4cW0xUT09"><strong>Panel: Startups and Entrepeneurship</strong></a>
-                        </div>
-                        <div className="dates">
+                        </div></td>
+                        </tr>
+                        <tr>
+                        <td><div className="dates grid-item">
                             <p><strong>Jan 23</strong></p>
-                        </div>
-                        <div className="times">
+                        </div></td>
+                        <td><div className="times grid-item">
                             <p>10:00 - 11:30</p>
-                        </div>
-                        <div className="links">
+                        </div></td>
+                        <td><div className="links grid-item">
                             <a href="https://us02web.zoom.us/j/84719148529?pwd=aEJDWVJ1ZGhLQzl3aS9Ock1vTldvdz09"><strong>Talk: Professor Srinath Sridhar</strong></a>
-                        </div>
+                        </div></td>
+                        </tr>
+                        </table>
                     </div>
                 </div>
         }
